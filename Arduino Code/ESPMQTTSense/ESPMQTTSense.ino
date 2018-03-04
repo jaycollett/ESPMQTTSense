3934fe8cd580831cfbe25297fc8d87d9ddfd4531
Jay Collett <486430+jaycollett@users.noreply.github.com>
2018-03-04 17:35:15 -0500

    initial version of firmware

--- Arduino Code/ESPMQTTSense/ESPMQTTSense.ino
+++ Arduino Code/ESPMQTTSense/ESPMQTTSense.ino
@@ -2,45 +2,47 @@
 #include <PubSubClient.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
-#include <Ticker.h>
 
-#define STATUS_DISCONNECTED "disconnected"
-#define STATUS_ONLINE "online"
-#define WIFI_SSID "<wifi-name>"
-#define WIFI_PASS "<wifi-password>"
-#define MQTT_IP "192.168.0.107"
+
+#define WIFI_SSID "xxxxx"
+#define WIFI_PASS "xxx"
 #define MQTT_PORT 1883
+char  mqtt_server[] = "192.168.0.0";
+char  mqtt_username[] = "xxxxx";
+char  mqtt_password[] = "xxxxxxx";
 
 ADC_MODE(ADC_VCC);
 
-unsigned long previousMillis = 0;
-const long interval = 10 * 60 * 1000;
+unsigned long previousPublishMillis = 0;        // store the last time we published data  
+const long publishInterval = 60000;             // interval (in MS) to update Adafruit IO data (1 min)
+unsigned long currentMillis;                    // store LOT mcu has been running
 
 const String baseTopic = "basementsensor";
 const String tempTopic = baseTopic + "/" + "temperature";
 const String humiTopic = baseTopic + "/" + "humidity";
 const String presTopic = baseTopic + "/" + "pressure";
-const String willTopic = baseTopic + "/" + "status";
 const String vccTopic  = baseTopic + "/" + "vcc";
-const String ipTopic   = baseTopic + "/" + "ip";
+
+char temperature[6];
+char humidity[6];
+char pressure[7];
+char vcc[10];
 
 IPAddress ip;
 
 WiFiClient WiFiClient;
-PubSubClient client(WiFiClient);
+PubSubClient mqttclient(WiFiClient);
 Adafruit_BME280 bme; // I2C
-Ticker ticker;
-
 
 void setup() {
   Serial.begin(115200);
-  delay(50);
+  delay(150);
 
   Wire.begin(0, 2);
   Wire.setClock(100000);
   if (!bme.begin(0x76)) {
     Serial.println("Could not find a valid BME280 sensor, check wiring!");
-    while (1);
+    //while (1);
   }
 
   Serial.println();
@@ -58,46 +60,30 @@ void setup() {
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.print("IP address: ");
-  ip = WiFi.localIP();
-  Serial.println(ip);
-
-  client.setServer(MQTT_IP, MQTT_PORT);
-  ticker.attach(600.0, publish);
+  mqttclient.setServer(mqtt_server, MQTT_PORT);
 }
 
 
-bool publishNow = false;
 void loop() {
+  
+  // yield for wifi 
   yield();
-  if (!client.connected()) {
-    Serial.println("Attempting MQTT connection...");
-    if (client.connect(chipId.c_str(), willTopic.c_str(), 1, true, STATUS_DISCONNECTED)) {
-      Serial.println("MQTT client connected.");
-      client.publish(willTopic.c_str(), STATUS_ONLINE, true);
-      client.publish(ipTopic.c_str(), ip.toString().c_str(), true);
-      publishNow = true;
-    } else {
-      Serial.print("failed, rc=");
-      Serial.println(client.state());
-    }
-  }
-
-  bmeRead();
-  vccRead();
-
-  if (client.connected()) {
-    client.loop();
-    if (publishNow) {
-        publish();
-        publishNow = false;
-    }
+  
+  // check to see if it's time to publish based on millis
+  currentMillis = millis();
+  if (currentMillis - previousPublishMillis >= publishInterval) {
+    bmeRead();
+    vccRead();
+    
+    MQTT_connect(); // connect to wifi/mqtt as needed
+    mqttclient.publish(tempTopic.c_str(), temperature);
+    mqttclient.publish(humiTopic.c_str(), humidity);
+    mqttclient.publish(presTopic.c_str(), pressure);
+    mqttclient.publish(vccTopic.c_str(), vcc);
+    
+    previousPublishMillis = currentMillis;
   }
 }
-
-char temperature[6];
-char humidity[6];
-char pressure[7];
-char vcc[10];
 
 void bmeRead() {
   float t = bme.readTemperature();
@@ -109,30 +95,40 @@ void bmeRead() {
   dtostrf(p, 5, 1, pressure);
 }
 
-void printMeasurements() {
-  Serial.print("t,h,p: ");
-  Serial.print(temperature); Serial.print(", ");
-  Serial.print(humidity);    Serial.print(", ");
-  Serial.print(pressure);    Serial.println();
-  Serial.print("vcc:   ");   Serial.println(vcc);
-}
-
-void publish() {
-  printMeasurements();
-
-  if (client.connected()) {
-    Serial.println("publishing values");
-    client.publish(tempTopic.c_str(), temperature);
-    client.publish(humiTopic.c_str(), humidity);
-    client.publish(presTopic.c_str(), pressure);
-    client.publish(vccTopic.c_str(), vcc);
-  } else {
-    Serial.println("client not connected, not publishing");
-  }
-}
-
 void vccRead() {
   float v  = ESP.getVcc() / 1000.0;
   dtostrf(v, 5, 1, vcc);
 }
 
+// #############################################################################
+//  mqtt Connect function
+//  This function connects and reconnects as necessary to the MQTT server and
+//  WiFi.
+//  Should be called in the loop to ensure connectivity
+// #############################################################################
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqttclient.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  // Loop until we're reconnected
+  while (!mqttclient.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (mqttclient.connect(mqtt_server, mqtt_username, mqtt_password)) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(mqttclient.state());
+      Serial.println(" try again in 2 seconds");
+      // Wait 2 seconds before retrying
+      delay(2000);
+    }
+  }
+  Serial.println("MQTT Connected!");
+}
